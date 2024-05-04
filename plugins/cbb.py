7b49ae1ea43564552aa58000b8bf8a76db626c15
Roshan78m <153927319+Roshan78m@users.noreply.github.com>
2024-05-04 07:15:08 +0530

    Update cbb.py

--- plugins/cbb.py
+++ plugins/cbb.py
@@ -23,7 +23,7 @@ async def cb_handler(client: Bot, query: CallbackQuery):
         )
     elif data == "about":
         await query.message.edit_text(
-            text = f"<b>Creator - Yuta_a_h\nMy Channel - Animation_hub_b\nDicsussion Group - animation_hub_gang</a>\n\n👨‍💻 Developed by Yuta_A_H</b>",
+            text = f"<b>Creator - @Spy_Radios\nMy Channel - @Animes_Paradox\nDicsussion Group - Paradox_bots</a>\n\n👨‍💻 Developed by @Spy_Radios</b>",
             disable_web_page_preview = True,
             reply_markup = InlineKeyboardMarkup(
                 [
